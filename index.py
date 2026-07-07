f89cc10a1b112541fc42ba7d21de16bc6e8530e2
北斗 <1912128646@qq.com>
2026-07-07 23:00:15 +0800

    字符串拼接

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
 print('hello world')
+
+# 字符串的转换
+s = 'hello world'
+print(s)
+
+ss = s.upper()
+print(ss)
+
+# 字符串的拼接
+s1 = 'hello'
+s2 = 'world'
+print(s1 + s2)
+
+# 字符串插入
+s3 = 'hello %s' % s2
+print(s3)
