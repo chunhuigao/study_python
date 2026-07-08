a795d7722e0490c053a8a8a2ed3c96e99e675106
北斗 <1912128646@qq.com>
2026-07-08 08:27:54 +0800

    类继承

--- class_name.py
+++ class_name.py
@@ -9,3 +9,23 @@ numberAdd = ClassName(1, 2)
 print(numberAdd.a)
 print(numberAdd.b)
 print(numberAdd.c)
+
+# 类的调用
+print(numberAdd.c)
+
+# 类的方法  
+def add(self):
+    return self.a + self.b
+
+# 类的属性
+print(numberAdd.a)
+print(numberAdd.b)
+print(numberAdd.c)
+
+# 类继承
+class ClassName2(ClassName):
+    def __init__(self, a, b):
+        super().__init__(a, b)
+        self.d = a * b
+        print(self.d)
+print(ClassName2(10, 2).d)
