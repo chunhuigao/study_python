bbcd412f05f0e04538c24a77abef75a4fbd88c61
北斗 <1912128646@qq.com>
2026-07-08 08:14:02 +0800

    数字的一下方法

---
+++ number.py
@@ -0,0 +1,47 @@
+# 数字的转换
+n = 10
+print(n)
+print(type(n))
+
+# 数字的比较
+n1 = 10
+n2 = 20
+print(n1 == n2)
+print(n1 != n2)
+print(n1 > n2)
+print(n1 < n2)
+print(n1 >= n2)
+print(n1 <= n2)
+
+# 数字的四舍五入
+n3 = 10.5
+print(round(n3))
+print(round(n3, 2)) 
+
+# 数字的取整
+n4 = 10.5
+print(int(n4))
+print(float(n4))
+
+# 数字的取余
+n5 = 10
+print(n5 % 3)
+
+# 数字的取整除
+print(n5 // 3)
+
+# 数字的取模
+print(n5 % 3)
+print(n5 ** 3)
+print(n5 ** 0.5)
+
+# 数字相加
+print(n5 + 3)
+print(n5 + 3.5)
+
+# 数字相减
+print(n5 - 3)
+print(n5 - 3.5)
+
+# 数字的转字符串
+print(str(n5))
