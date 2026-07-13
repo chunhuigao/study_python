76a436d9886d8511c93ed7b85d22b2fe0ea28339
北斗 <1912128546@qq.com>
2026-07-13 22:26:34 +0800

    对称二叉树

---
+++ leetcode/101.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        return self.isMirror(root.left,root.right)
+    def isMirror(self, left: Optional[TreeNode], right: Optional[TreeNode]) -> bool:
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        return left.val == right.val and self.isMirror(left.left,right.right) and self.isMirror(left.right,right.left)
