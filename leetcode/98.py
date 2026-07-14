ba1c35f9f47c8d001c003d817ca496c624f2792f
北斗 <1912128546@qq.com>
2026-07-14 08:25:41 +0800

    验证二叉搜索树

---
+++ leetcode/98.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode],left = -inf,right = inf) -> bool:
+        if root is None:
+            return True
+        val = root.val
+        return left < val  < right and self.isValidBST(root.left,left,val) and self.isValidBST(root.right,val,right)
+        
